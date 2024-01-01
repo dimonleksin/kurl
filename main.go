3ec24265b8a1c7b7ec617092a5317f359096aa58
0142 <leksindmitri@gmail.com>
2024-01-01 18:03:37 +0400

    Done

--- main.go
+++ main.go
@@ -1,30 +1,101 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"log"
+	"time"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/segmentio/kafka-go"
+)
+
+const retries = 3
+
+var (
+	bootstrap_server *string
+	action           *string
+	topic            *string
+	msg              string
 )
 
 func main() {
-
-	// k := flag.String("test", "def_val", "# My fearst test flag")
-	// k2 := flag.Int("count", -1, "# My second test flag")
-	// flag.Parse()
-	// flag.Set("t2", "4")
-	// fmt.Println(*k, *k2)
-	// client, err := kafka.NewConsumer(&kafka.ConfigMap{
-
-	// })
-	bootstrap_server := flag.String(
+	// get Bootstrap server from flag
+	bootstrap_server = flag.String(
 		"bootstrap_server",
-		"127.0.0.1",
-		"Bootstrap server anf port (kafka1:9092) of kafka")
+		"127.0.0.1:9092",
+		"Bootstrap server anf port (kafka1:9092) of kafka",
+	)
+	action = flag.String(
+		"action",
+		"read",
+		"Who action you need(read/write)",
+	)
+	topic = flag.String(
+		"topic",
+		"",
+		"Name of topic to read/write",
+	)
 
 	flag.Parse()
-	cfg := &kafka.ConfigMap{
-		"bootstrap_servers": *bootstrap_server,
+	if *action == "read" {
+		r := kafka.NewReader(kafka.ReaderConfig{
+			Brokers:  []string{*bootstrap_server},
+			GroupID:  "consumer-group-id",
+			Topic:    *topic,
+			MaxBytes: 10e6, // 10MB
+		})
+
+		for {
+			m, err := r.ReadMessage(context.Background())
+			if err != nil {
+				log.Fatal("Error read messages")
+				break
+			}
+			fmt.Printf("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+		}
+
+		if err := r.Close(); err != nil {
+			log.Fatal("failed to close reader:", err)
+		}
+	} else if *action == "write" {
+		w := &kafka.Writer{
+			Addr:                   kafka.TCP(*bootstrap_server),
+			Topic:                  *topic,
+			AllowAutoTopicCreation: false,
+		}
+
+		var err error
+
+		for {
+			_, err = fmt.Scan(&msg)
+			if err != nil {
+				panic("Error read text from srdin")
+			}
+			messages := kafka.Message{
+				Value: []byte(msg),
+			}
+			for i := 0; i < retries; i++ {
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+
+				// attempt to create topic prior to publishing the message
+				err = w.WriteMessages(ctx, messages)
+				if errors.Is(err, kafka.LeaderNotAvailable) || errors.Is(err, context.DeadlineExceeded) {
+					time.Sleep(time.Millisecond * 250)
+					continue
+				}
+
+				if err != nil {
+					log.Fatalf("unexpected error %v", err)
+				}
+				break
+			}
+
+		}
+		// if err := w.Close(); err != nil {
+		// 	log.Fatal("failed to close writer:", err)
+		// }
 	}
-	fmt.Println(*cfg)
 }
