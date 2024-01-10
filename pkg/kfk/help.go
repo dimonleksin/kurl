4772f1c8804c7bf8219c729037a2b8973a0d790c
dimonleksin <86394903+dimonleksin@users.noreply.github.com>
2024-01-10 12:05:50 +0400

    Update help.go

--- pkg/kfk/help.go
+++ pkg/kfk/help.go
@@ -6,8 +6,8 @@ func PrintHelp() {
 	textHelp := "--bootstrap_server [string] for set addres of brokers\n" +
 		"\t format: host:port, like 127.0.0.1:9092\n\n" +
 		"--topic [string] for set topic name for read/write\n\n" +
-		"--action [string] for set< whot u need (read/write)\n\n" +
-		"--user [string] set username, if u dont set this arg, use PLAINTEXT\n" +
+		"--action [string] for set what u need (read/write)\n\n" +
+		"--user [string] set username, if u dont set this arg, used PLAINTEXT\n" +
 		"\tif set --user, u need set and --password\n\n" +
 		"--password [string] set password for connect to kafka\n" +
 		"\tif u set password without --user, this call panic\n\n" +
